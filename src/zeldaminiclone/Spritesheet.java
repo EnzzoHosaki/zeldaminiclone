8bceb9c396187db0567fc44b536cb0c69f8f638c
Enzzo Hosaki <Enzzohosaki@hotmail.com>
2023-12-11 13:49:51 -0300

    Primeiro commit

---
+++ src/zeldaminiclone/Spritesheet.java
@@ -0,0 +1,30 @@
+package zeldaminiclone;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Spritesheet {
+	
+	public static BufferedImage spritesheet;
+	
+	public static BufferedImage player_front;
+	
+	public static BufferedImage tileWall;
+	
+	public Spritesheet(){
+		try {
+			spritesheet = ImageIO.read(getClass().getResource("/spritesheet.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		player_front = Spritesheet.getSprite(0, 11, 16, 16);
+		tileWall = Spritesheet.getSprite(190, 154, 16, 16);
+	}
+	
+	public static BufferedImage getSprite(int x, int y, int width, int height) {
+		return spritesheet.getSubimage(x, y, width, height);
+	}
+}
